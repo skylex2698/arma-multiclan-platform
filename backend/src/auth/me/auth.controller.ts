68f2dd446849de30f2f1f7472d88e88d795655aa
skylex2698 <skylex26.1998@gmail.com>
2026-01-19 09:59:24 +0100

    Borrado y paso 5 de la fase 1

--- backend/src/auth/me/auth.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@UseGuards(AuthGuard('jwt'))
-@Get('me')
-me(@Req() req) {
-  return req.user;
-}
